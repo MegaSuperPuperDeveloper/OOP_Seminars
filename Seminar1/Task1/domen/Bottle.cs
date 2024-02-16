39e385b433f7fd05fb0369778ec7ba34c0e13e6a
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-02-16 19:22:31 +0300

    V1

---
+++ Seminar1/Task1/domen/Bottle.cs
@@ -0,0 +1,22 @@
+class Bottle : Product
+{
+    private double volume;
+
+    public Bottle(long id, string name, double price, int place, double volume) : base(id, name, price, place)
+    {
+        this.volume = volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public string toString() {
+        return $"Bottle [{base.toString()}, volume={volume}]";
+    }
+
+}
